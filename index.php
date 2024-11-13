16fa0f625df8a2c915b756c1fdb2a51d7038048f
Amine-Bouguettaya <aminebouguettaya5@gmail.com>
2024-11-13 16:15:03 +0100

    ajout de l'affichage des chambres d'un hotel avec un peu de css.

--- index.php
+++ index.php
@@ -13,14 +13,14 @@ $client4 = new Client ("Madame", "Madame");
 
 $hotel1 = new Hotel ("Hilton", "10 route de la Gare", "67000", "Strasbourg");
 $hotel2 = new Hotel ("Hôtel Regent", "1 rue des Abeilles", "Paris", "Paris");
-$chambre1 = new Chambre ($hotel1, 1, 100, 1, false);
-$chambre2 = new Chambre ($hotel1, 2, 100, 1, false);
-$chambre3 = new Chambre ($hotel1, 3, 100, 1, false);
-$chambre4 = new Chambre ($hotel1, 4, 100, 1, true);
-$chambre5 = new Chambre ($hotel2, 1, 100, 1, false);
-$chambre6 = new Chambre ($hotel2, 2, 100, 1, false);
-$chambre7 = new Chambre ($hotel2, 3, 100, 1, true);
-$chambre8 = new Chambre ($hotel2, 4, 100, 1, true);
+$chambre1 = new Chambre ($hotel1, 1, 100, true, true);
+$chambre2 = new Chambre ($hotel1, 2, 100, true, true);
+$chambre3 = new Chambre ($hotel1, 3, 100, false, true);
+$chambre4 = new Chambre ($hotel1, 4, 100, false, true);
+$chambre5 = new Chambre ($hotel2, 1, 100, false, true);
+$chambre6 = new Chambre ($hotel2, 2, 100, false, true);
+$chambre7 = new Chambre ($hotel2, 3, 100, true, true);
+$chambre8 = new Chambre ($hotel2, 4, 100, true, true);
 
 $reservation1 = new Reservation ($chambre1, $client1, "12-11-2024", "24-11-2024");
 $reservation2 = new Reservation ($chambre2, $client1, "12-12-2024", "24-12-2024");
@@ -37,3 +37,64 @@ echo "<br><br>";
 echo $hotel1->afficherReservationHotel();
 echo "<br><br>";
 echo $hotel2->afficherReservationHotel();
+
+echo "<br><br>";
+echo "<br><br>";
+
+echo afficherChambreHTML($hotel1);
+
+function afficherChambreHTML($hotel) {
+    $result = "
+    <style>
+    table {
+        border-collapse: collapse;
+    }
+    td {
+        padding: 15px;
+        text-align: left;
+        border-bottom: 1px solid #000000;
+    }
+    th {
+        padding: 15px;
+        text-align: left;
+        border-bottom: 1px solid #000000;
+        background-color: #f4f4f4;
+        font-weight: bold;
+    }
+    </style>
+    <br>
+    <table border=1>
+                <thead>
+                    <tr>
+                        <th>Chambre</th>
+                        <th>Prix</th>
+                        <th>Wifi</th>
+                        <th>Etat</th>
+                    </tr>
+                </thead>
+                <tbody>";
+    $wifi ="";
+    $disponibilite = "";
+    foreach($hotel->getTab() as $chambre) {
+        if ($chambre->getWifi() == true) {
+            $wifi = "<img src='img/wifi.png'></i>";
+        }
+        else {
+            $wifi = "";
+        }
+        if ($chambre->getDisponibilite() == true) {
+            $disponibilite = "<p style='background-color:DarkSeaGreen; color:white;'>Disponible</p>";
+        }
+        else {
+            $disponibilite = "<p style='background-color:LightCoral; color:white;'>Reservé</p>";
+        }
+        $result .= "<tr>
+                        <td>Chambre ".$chambre->getNchambre()."</td>
+                        <td>".$chambre->getPrix()." €</td>
+                        <td>".$wifi."</td>
+                        <td>".$disponibilite."</td>
+                    </tr>";
+    }
+    $result .= "</tbody></table>";
+    return $result;
+}
